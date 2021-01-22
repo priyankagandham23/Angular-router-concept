6100fbb3d17aea4cbb48e80956638b918d339489
priyankagandham23 <priyanka.gandham@nirmaan.org>
2021-01-22 13:29:58 +0530

    2 files modified

--- app/views/home/home.component.ts
+++ app/views/home/home.component.ts
@@ -6,7 +6,7 @@ import { Http } from '@angular/http';
   template: `
     <div>
       <h1>Home</h1>
-      <h3>Total users: # {{users?.length}}</h3>
+      <h3>Total users: # {{users?.length}} </h3><p>hello</p>
     </div>
   `,
 })
--- app/views/login/login.component.ts
+++ app/views/login/login.component.ts
@@ -4,7 +4,10 @@ import { Http } from '@angular/http';
 @Component({
   selector: 'login',
   template: `
-    <h1>Login</h1>
+    <h1>Login</h1>'
+          <h1> Hello </h1>
+            <p [name]="v"></p>
+
   `,
 })
 export class LoginViewComponent {
